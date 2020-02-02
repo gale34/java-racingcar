03f04a618a4bb721854d71e1a635a17cd65d6c52
gale34 <gales34@gmail.com>
2020-02-03 08:57:18 +0900

    feat: add Webmain and index.html

--- src/main/java/WebMain.java
+++ src/main/java/WebMain.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+import static spark.Spark.*;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.Map;
 
 public class WebMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        port(8080);
+        get("/", (req, res) -> render(null,"/index.html"));
+/*        String carNames = inputCarNames();
+        Integer roundCount = inputRoundCount();
+
+        RacingGame racingGame = new RacingGame(carNames, roundCount);
+        RacingGameView racingGameView = new RacingGameConsoleView();
+
+        registerRacingGameEvents(racingGameView);
+        racingGameView.showGameProgressGuidanceMessage();
+        racingGame.start();
+
+        Events.reset();*/
+    }
+
+    public static String render(Map<String, Object> model, String templatePath) {
+        return new HandlebarsTemplateEngine()
+                .render(new ModelAndView(model, templatePath));
+    }
+
+    /*private static void registerRacingGameEvents(RacingGameView racingGameView) {
+        Events.handle((RoundOverEvent event) -> racingGameView.showCurrentPosition(event.getCarPositions()));
+        Events.handle((GameOverEvent event) -> racingGameView.showGameResult(event.getWinners()));
+    }*/
 }
